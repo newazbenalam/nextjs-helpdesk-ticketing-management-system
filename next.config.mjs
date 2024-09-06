e49766d114e68fd3480aaecd051457d6e6549151
newazbenalam <newazbenalam@gmail.com>
2024-09-07 01:24:31 +0600

    FIX: export standalone

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,40 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: false
-
+  poweredByHeader: false,
+  cleanDistDir: true,
+  crossOrigin: 'anonymous',
+  env: {
+    HOSTNAME: 'localhost',
+  },
+  logging: {
+    level: 'info',
+  },
+  reactStrictMode: false,
+  // experimental: {
+  //   missingSuspenseWithCSRBailout: false,
+  // },
+  // distDir: 'build',
+  output: 'standalone',
+  //output: 'export',
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  headers: () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'powerBy',
+            value: 'Neo',
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
